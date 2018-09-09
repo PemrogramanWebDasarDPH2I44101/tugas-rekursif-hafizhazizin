8b9612bcfa1e4e784c3db1a70716369d788ad5d3
Hafizh 'Azizi Nasution <42725297+hafizhazizin@users.noreply.github.com>
2018-09-09 12:26:09 +0700

    Update rekursif.php

--- rekursif.php
+++ rekursif.php
@@ -1,10 +1,43 @@
-<?php
+<p>Masukan Angka</p>
+<form method="Post">
+	Angka: <input type="number" name="a"> 
+	<input type="submit" name="submit" value="submit"> <br>
+</form>
+<?php 
+	$a =$_POST['a'];
 
-function rekursif($i){
-  echo "$i <br>";
-  $i--;
-  if($i>0)
-    rekursif($i);
-}
-rekursif(5);
-?>
+	function rekursif($z,$x){
+	 	if ($z > 0) {
+	 		if ($x > 0) {
+	 			echo "* ";
+	 			$x--;
+	 			rekursif($z,$x);
+	 		}	
+	 		else{
+	 			echo "<br>";
+	 			$z--;
+	 			rekursif($z,$z);
+	 		}
+	 	}
+	 }
+	rekursif ($a, $a);
+
+echo "============================================= <br>";
+
+	function rekursif2($z,$x,$a){
+	 	if ($z <= $a) {
+	 		if ($x <= $z) {
+	 			echo "$x ";
+	 			$x++;
+	 			rekursif2($z,$x,$a);
+	 		}	
+	 		else{
+	 			echo "<br>";
+	 			$z++;
+	 			rekursif2($z,1,$a);
+	 		}
+	 	}
+	}
+	rekursif2 (1,1,$a);
+
+ ?>
